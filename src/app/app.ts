26cb7b4b03ca2e2a083fdd1e21949225a8beceab
heroelkinggg <medoelking2009@gmail.com>
2026-09-01 13:48:19 -0700

    chore: clean up scaffold

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,11 @@
-import { Component, inject } from '@angular/core';
-import { AsyncPipe } from '@angular/common';
-import { FormService } from './form.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [AsyncPipe],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  private formService = inject(FormService);
-
-  forms = this.formService.getForms();
+  protected title = 'registry-app';
 }
